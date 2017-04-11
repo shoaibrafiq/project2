8f1d5c13da54bb446692e63b8a3f4871028ecc18
shoaibrafiq <u1464884@unimail.hud.ac.uk>
2017-04-11 15:27:38 +0100

    Made navigation bar and added sign up and login links, user can regiter, login and then sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
 resources :films
 root 'films#index'
 #instead of declaring seprate routes for the index, show, new, edit update and destroy actions, a resourceful route declares them in a single line of code
