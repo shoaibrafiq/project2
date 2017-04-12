badde79ec8974a214e3124780f0657325c3a8298
shoaibrafiq <u1464884@unimail.hud.ac.uk>
2017-04-12 12:54:24 +0100

    Defined Relationships between film and Genre in models

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,4 +1,6 @@
 class Film < ActiveRecord::Base
 belongs_to :user
 # A film  belongs to a user
+belongs_to :Genre
+# One film belongs to one genre
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,4 @@
 class Genre < ActiveRecord::Base
+has_many :films
+#one genre can have many films
 end
