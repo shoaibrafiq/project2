6a4119e6f038963341fdcc2cac5e6d5a811fc61b
shoaibrafiq <u1464884@unimail.hud.ac.uk>
2017-04-12 12:49:30 +0100

    Added Genre Model and database table also connnected films table with genre table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412104916) do
+ActiveRecord::Schema.define(version: 20170412114642) do
 
   create_table "films", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20170412104916) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.integer  "genre_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
