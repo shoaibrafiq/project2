0699e99777181dc4c9986ad6aaa8ed25ca7c18c9
shoaibrafiq <u1464884@unimail.hud.ac.uk>
2017-04-18 13:00:27 +0100

    migrated reviews table also film_id and user_id is added to the table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170415143449) do
+ActiveRecord::Schema.define(version: 20170418121512) do
 
   create_table "films", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 20170415143449) do
     t.datetime "updated_at",  null: false
     t.string   "description"
     t.integer  "user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer  "rating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.integer  "film_id"
   end
 
   create_table "users", force: :cascade do |t|
