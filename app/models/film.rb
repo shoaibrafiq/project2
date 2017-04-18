ad5df421e79ff5b11cf32018d5922fc0f460f829
shoaibrafiq <u1464884@unimail.hud.ac.uk>
2017-04-18 13:24:16 +0100

    Defined relationships in models between user, film and review

--- app/models/film.rb
+++ app/models/film.rb
@@ -3,7 +3,7 @@ belongs_to :user
 # A film  belongs to a user
 belongs_to :genre
 # One film belongs to one genre
-
+has_many :reviews
 has_attached_file :film_img, styles: { film_index: "250x350>", film_show: "325x475>" }, default_url: "/images/:style/missing.png"
  validates_attachment_content_type :film_img, content_type: /\Aimage\/.*\z/
  #defining the patched file for image upload
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   has_many :films # A user can have many films
   has_many :genres
+  has_many :reviews
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
